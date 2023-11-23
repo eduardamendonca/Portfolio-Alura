446c9e0643a375e5b23bbd0cbcef445deb770fed
Eduarda Mendonça <eduardamendonca.dev@gmail.com>
2023-11-23 20:10:39 -0300

    0.0.2

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+
+//animação da página habilidades
+
+function mouseOuverSQL(){
+    document.getElementById("mouseAlt").innerHTML = "O SQL é uma linguagem padrão para manipulação de registros em bancos de dados relacionais";
+}
+function mouseOutSQL(){
+    document.getElementById("mouseAlt").innerHTML = "Passe o mouse para descorbrir algumas das minhas habilidades"
+}
+function mouseOuverPHP(){
+    document.getElementById("mouseAlt").innerHTML = "É uma linguagem de script open source de uso geral, muito utilizada, e especialmente adequada para o desenvolvimento web e que pode ser embutida dentro do HTML";
+}
+function mouseOutPHP(){
+    document.getElementById("mouseAlt").innerHTML = "Passe o mouse para descorbrir algumas das minhas habilidades"
+}
+function mouseOuverHTML(){
+    document.getElementById("mouseAlt").innerHTML = "HTML é uma uma linguagem de marcação de hipertexto significa ser capaz de escrever páginas interligadas ";
+}
+function mouseOutHTML(){
+    document.getElementById("mouseAlt").innerHTML = "Passe o mouse para descorbrir algumas das minhas habilidades"
+}
+function mouseOuverJAVASCRIPT(){
+    document.getElementById("mouseAlt").innerHTML = "Javascript é uma linguagem de programação de alta complexidade, mas de fácil uso, voltada para criar elementos em aplicações web, como sites, aplicativos e sistemas online.";
+}
+function mouseOutJAVASCRIPT(){
+    document.getElementById("mouseAlt").innerHTML = "Passe o mouse para descorbrir algumas das minhas habilidades"
+}
+function mouseOuverCPP(){
+    document.getElementById("mouseAlt").innerHTML = "C++ é uma das linguagens mais versáteis que existem, permitindo desenvolver desde tarefas simples como aplicações na linha de comando ou web, até sistemas complexos de tempo real, muito usadas no mercado financeiro.";
+}
+function mouseOutCPP(){
+    document.getElementById("mouseAlt").innerHTML = "Passe o mouse para descorbrir algumas das minhas habilidades"
+}
+function mouseOuverCSS(){
+    document.getElementById("mouseAlt").innerHTML = "O CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML.";
+}
+function mouseOutCSS(){
+    document.getElementById("mouseAlt").innerHTML = "Passe o mouse para descorbrir algumas das minhas habilidades"
+}
